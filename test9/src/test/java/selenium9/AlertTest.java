f4304f5222f3a073b4771fdc4d2ecb5a56718ddd
Szymon Szewczyk <walkiri@gmail.com>
2018-11-09 19:16:32 +0100

    dodano clickpromptboxtext

--- test9/src/test/java/selenium9/AlertTest.java
+++ test9/src/test/java/selenium9/AlertTest.java
@@ -1,12 +1,10 @@
 package selenium9;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -22,13 +20,33 @@ public class AlertTest {
 
     @BeforeClass
     public static void classSetup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     @Before
     public void testSetup() {
+        driver.get("https://www.seleniumeasy.com/test/javascript-alert-box-demo.html");
     }
 
     @AfterClass
     public static void classTeardown() {
+        driver.quit();
     }
+
+    @Test
+    public void clickPromptBoxText(){
+        WebElement el =  driver.findElement(By.xpath("//button[contains(text(),'Click for Prompt Box')]"));
+        el.click();
+        assertEquals("Enter name",driver.switchTo().alert().getText() );
+
+        driver.switchTo().alert().accept();
+
+            try {
+                Thread.sleep(5555);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+    }
+
 }
