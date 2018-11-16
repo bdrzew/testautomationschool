d439ef9802207ff26a29fc4723a799acd3850c69
Bartosz Drzewinski <bartosz.drzewinski.ctr@sabre.com>
2018-11-16 15:27:08 +0100

    Zajecia 6 empty project

---
+++ zajecia6/src/test/java/zajecia6/FormTest.java
@@ -0,0 +1,26 @@
+package zajecia6;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * This class is designed to test 2 forms available at
+ * https://www.seleniumeasy.com/test/javascript-alert-box-demo.html
+ */
+public class FormTest {
+
+    @BeforeClass
+    public void classSetup() {
+    }
+
+    @BeforeMethod
+    public void testSetup() {
+
+    }
+
+    @AfterClass
+    public void terdownClass() {
+
+    }
+}
