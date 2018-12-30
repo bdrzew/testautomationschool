a43c849bc68d85c60159a426228fe3fa1a35dce8
Adam Moskal <adammoskal7@gmail.com>
2018-12-31 00:33:24 +0100

    modify

--- adam_zadanie10/src/test/java/zadanie10/assertion/LoginAssertion.java
+++ adam_zadanie10/src/test/java/zadanie10/assertion/LoginAssertion.java
@@ -1,7 +1,15 @@
 package zadanie10.assertion;
 
-import org.testng.asserts.Assertion;
+import zadanie10.assertion.common.Assertion;
 import zadanie10.page.LoginPage;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
+
 
 public class LoginAssertion extends Assertion<LoginPage> {
+
+    public  LoginAssertion IsLoginPageIsLoaded() {
+        //assertTrue(page.waitUntil(condition -> page.getEmail().isDisplayed());
+
+    }
 }
